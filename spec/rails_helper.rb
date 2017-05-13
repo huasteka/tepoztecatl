30e3a04750c099f57d76bd8f42a89c366f503edf
Murilo Costa <murilo.nunes.jose@gmail.com>
2017-05-12 22:28:45 -0300

    Removing IDE configuration folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,7 @@ require 'database_cleaner'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-Shoulda::Matchers.configura do |config|
+Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
