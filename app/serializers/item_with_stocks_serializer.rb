06fb5689c71acf74fb24147d5a6ff6cc895c8184
Murilo Costa <murilo.nunes.jose@gmail.com>
2018-03-25 16:28:44 -0300

    Fix #8

--- app/serializers/item_with_stocks_serializer.rb
+++ app/serializers/item_with_stocks_serializer.rb
@@ -1,5 +1,5 @@
 class ItemWithStocksSerializer < ActiveModel::Serializer
-  attributes :code, :name
+  attributes :code, :name, :input_quantity, :output_quantity
 
   belongs_to :input_measure_unit
   belongs_to :output_measure_unit
