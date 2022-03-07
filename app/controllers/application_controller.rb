61c6e4e7bb53eb81a3eca27b199f456b023b5d3c
Murilo Costa <murilo.nunes.jose@gmail.com>
2022-03-07 22:30:14 +0100

    GH-31 Fix 404 error redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
 
+  def routing_error
+    render :json => {:errors => [{:status => 404, :title => 'Not Found'}]}, :status => :not_found
+  end
 end
