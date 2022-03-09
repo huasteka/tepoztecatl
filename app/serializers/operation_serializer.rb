b10618c83159ab86352aa76b2861f7fbcf8154cd
Murilo Costa <murilo.nunes.jose@gmail.com>
2022-03-10 00:14:29 +0100

    GH-34 Create fetch stock operations history

---
+++ app/serializers/operation_serializer.rb
@@ -0,0 +1,37 @@
+=begin
+  @apiDefine OperationModel
+  @apiSuccess {Object} data
+  @apiSuccess {Number} data.id
+  @apiSuccess {String} data.type
+  @apiSuccess {String} data.attributes.operation_type
+  @apiSuccess {Number} data.attributes.quantity
+  @apiSuccess {Object} data.attributes.stock
+  @apiSuccess {Number} data.attributes.stock.minimum_quantity
+  @apiSuccess {Object} data.attributes.stock.storage
+  @apiSuccess {Number} data.attributes.stock.storage.id
+  @apiSuccess {String} data.attributes.stock.storage.name
+  @apiSuccess {String} data.attributes.stock.storage.code
+  @apiSuccess {Object} data.attributes.stock.item
+  @apiSuccess {Number} data.attributes.stock.item.id
+  @apiSuccess {String} data.attributes.stock.item.name
+  @apiSuccess {String} data.attributes.stock.item.code
+=end
+class OperationSerializer < ActiveModel::Serializer
+  attributes :operation_type, :quantity
+
+  attribute :stock do
+    {
+        minimum_quantity: object.stock.minimum_quantity,
+        storage: {
+            id: object.stock.storage.id,
+            name: object.stock.storage.name,
+            code: object.stock.storage.code,
+        },
+        item: {
+            id: object.stock.item.id,
+            name: object.stock.item.name,
+            code: object.stock.item.code,
+        }
+    }
+  end
+end
